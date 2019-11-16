d3b7f0e75cbf9fef88c3c5c60737c7a7a51f2c8a
Felix-Suen <felix.suen2000@gmail.com>
2019-11-16 15:24:31 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,49 +1,24 @@
 import React from 'react';
 import './App.css';
-
-const chara = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+import Pwgen from './components/pwgen';
+import Chat from './components/chat';
+import Login from './components/login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      Password: 'du hello',
-    }
-  }
-  
-  onClickButton = () => {
-    var stringer = '';
-    for (var i = 0; i < 16; i++) {
-      stringer += chara[Math.floor(Math.random() * 92) + 1];
-    }
-    this.setState({
-      Password: stringer,
-    });
-    document.getElementById('copy').style.visibility = "visible";
-  } 
-
-  onCopyButton = (str) => {
-    const el = document.createElement('textarea');
-    el.value = str;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-  }
-
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <button className="button" onClick={this.onClickButton}>Gen Password</button>
-          <p>{this.state.Password}</p>
-          <button className="copy" id="copy" onClick={this.onCopyButton(this.state.Password)}>Copy</button>
-        </header>
-      </div>
+    return(
+      <Router>
+        <div>
+          <Switch>
+            <Route path='/pwgen' component={Pwgen}/>
+            <Route path='/chat' component={Chat}/>
+            <Route path='/' exact component={Login}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
-
 }
 
 export default App;
---
+++ src/components/pwgen.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './pwgen.css';
+
+const chara = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+class Pwgen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      Password: 'du hello',
+    }
+  }
+  
+  onClickButton = () => {
+    var stringer = '';
+    for (var i = 0; i < 16; i++) {
+      stringer += chara[Math.floor(Math.random() * 92) + 1];
+    }
+    this.setState({
+      Password: stringer,
+    });
+    document.getElementById('copy').style.visibility = "visible";
+  } 
+
+  onCopyButton = (str) => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button className="button" onClick={this.onClickButton}>Gen Password</button>
+          <p>{this.state.Password}</p>
+          <button className="copy" id="copy" onClick={this.onCopyButton(this.state.Password)}>Copy</button>
+        </header>
+      </div>
+    );
+  }
+
+}
+
+export default Pwgen;
