0efd0be7fe9dded58458555385ded57aa378ab04
Felix Suen <felix.suen2000@gmail.com>
2019-11-19 10:48:53 -0500

    chat navbar

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import './chat.css';
 
 class Chat extends React.Component {
   render() {
     return(
-      <h1 style={{color: 'white', paddingTop: '200px', textAlign: 'center'}}>GOOD MORNING GAMERS</h1>
+      <div>
+        <ul>
+          <li className="user"><a>MA NAME {this.props.user.toUpperCase()}</a></li>
+          <li><a onClick={() => this.props.history.push('/pwgen')}>Gen Password</a></li>
+          <li><a href='https://github.com/al-jshen/chat' target="_blank">Source Code</a></li>
+          <li><a>Log Out</a></li>
+        </ul>
+      </div>
     );
   }
 }
 
-export default Chat;
+const mapStateToProps = (state) => {
+  return {
+      user: state.user,
+      islogged: state.islogged
+  }
+};
+
+export default connect(mapStateToProps)(Chat);
