5cdb952b4e9cbd4aa918d166046c0c2bda77d185
Jeff Shen <jshen@protonmail.ch>
2019-11-20 22:08:55 -0500

    moved chat to chat.js

--- src/App.js
+++ src/App.js
@@ -6,33 +6,6 @@ import Login from './components/login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            'text': '',
-            'list': [],
-        }
-    }
-
-    handleChange = (e) => {
-        this.setState({
-            'text': e.target.value
-        })
-    }
-
-    handleKeyPress = (e) => {
-        if (e.key == "Enter") {
-            this.handleAdd()
-        }
-    }
-
-    handleAdd = () => {
-        this.setState({
-            'list': [...this.state.list, this.state.text],
-            'text': ''
-        })
-    }
-
   render() {
     return(
       <Router>
@@ -43,14 +16,7 @@ class App extends React.Component {
             <Route path='/login' component={Login}/>
           </Switch>
         </div>
-        <div>
-            <input type="text" value={this.state.text} onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>  
-            <button onClick={this.handleAdd}>Add whatever to list</button>
-        </div>
-        {this.state.list.map((item, index) => {
-            return(<div key={index}>{item}</div>)
-        })}
-      </Router>
+     </Router>
     );
   }
 }
