70e67207dfdc06e92d647dbcb26c3c48fa63ccb3
Jeff Shen <jshen@protonmail.ch>
2019-11-21 18:25:05 -0500

    saved login session

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -21,8 +21,29 @@ class Login extends React.Component {
             [e.target.name]: e.target.value
         });
     }
+        
+    componentDidMount() {
+        const username = localStorage.getItem('username');
+        const password = localStorage.getItem('password');
+        if (username && password) {
+            this.setState({
+                username: username,
+                password: password
+            }, () => {
+                this.handleSuccess()
+            })
+        }
+    }
 
-    handleSubmit = (e) => {
+    handleSuccess = (e) => {
+        localStorage.setItem('username', this.state.username);
+        localStorage.setItem('password', this.state.password);
+        this.props.rename(this.state.username);
+        this.props.changelog(true);
+        this.props.history.push('/');
+    }
+
+    handleLogin = (e) => {
         e.preventDefault()
         const username = this.state.username;
         const password = this.state.password;
@@ -33,9 +54,7 @@ class Login extends React.Component {
         .then((res) => {
             console.log(res.data);
             if (res.data) {
-                this.props.rename(this.state.username);
-                this.props.changelog(true);
-                this.props.history.push('/');
+                this.handleSuccess();
             } else {
                 this.setState({
                     error: "FAILED TO AUTHENTICATE"
@@ -47,7 +66,7 @@ class Login extends React.Component {
         })
         .catch((err) => {
             this.setState({
-                error: "Wrong boi"
+                error: "AUTHENTICATION ERROR"
             })
         })
     }
@@ -56,7 +75,7 @@ class Login extends React.Component {
         return(
             <div className="form">
                 <h1>Gotta Go Faster</h1>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleLogin}>
                     <div className="container">
                         <label>
                             <input 
